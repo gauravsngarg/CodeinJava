e9ebf61868f0a17310cf7ddf274b35e6afbb5315
Gaurav Garg <gauravsngarg@gmail.com>
2017-06-10 15:25:09 +0530

    Removing common main()

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,12 @@
-//http://www.geeksforgeeks.org/fill-array-1s-minimum-iterations-filling-neighbors/
 
 public class Solution {
 
-	public static void main(String[] args) {
-		int[] arr = {1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1};
-		FillOnesinArray ob = new FillOnesinArray();
+//	public static void main(String[] args) {
+//		CompleteCircleWithFuelDistance ob = new CompleteCircleWithFuelDistance();
+//		ob.arr[0];
+//		
+//		
+//		
+//	}
 
-		ob.init(arr);
-		int ans = ob.fillOnesInArray(arr);
-		System.out.print(ans);
-		
-		
-	}
 }
