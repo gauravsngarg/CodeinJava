3e7bce5c68dbc7cf2fc979847dcea3054f52c46a
Gaurav Garg <gauravsngarg@gmail.com>
2017-06-10 11:56:29 +0530

    Adding comments before writing algo

--- src/FillOnesinArray.java
+++ src/FillOnesinArray.java
@@ -1,4 +1,14 @@
 
 public class FillOnesinArray {
-
+	
+	//3 cases
+	//Case 1: Check for all blocks of zero keeping 1's on both side
+		//if(count_zero == even) print count_zero/2
+		//if(count_zero == odd) print (count_zero/2)+1
+	//Case 2: Check for all blocks of 0's before first one
+		//print count_zero
+	//Case 3: Check for all blocks of 0's after last one
+		//print count_zero
+	
+	
 }
--- src/Solution.java
+++ src/Solution.java
@@ -2,4 +2,11 @@
 
 public class Solution {
 
+	public static void main(String[] args) {
+		int[] arr = {0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1};
+		
+		
+		
+		
+	}
 }
