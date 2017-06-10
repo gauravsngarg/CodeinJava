63c4f8a436f48ab21c249425d8308ca4061bc037
Gaurav Garg <gauravsngarg@gmail.com>
2017-06-10 12:24:20 +0530

    Implemented Algo O(n) to calculate minimum no. of iterations Space O(1)

--- src/FillOnesinArray.java
+++ src/FillOnesinArray.java
@@ -1,14 +1,67 @@
 
 public class FillOnesinArray {
-	
-	//3 cases
-	//Case 1: Check for all blocks of zero keeping 1's on both side
-		//if(count_zero == even) print count_zero/2
-		//if(count_zero == odd) print (count_zero/2)+1
-	//Case 2: Check for all blocks of 0's before first one
-		//print count_zero
-	//Case 3: Check for all blocks of 0's after last one
-		//print count_zero
-	
-	
+
+	// 3 cases
+	// Case 1: Check for all blocks of 0's before first one
+	// print count_zero
+	// Case 2: Check for all blocks of zero keeping 1's on both side
+	// if(count_zero == even) print count_zero/2
+	// if(count_zero == odd) print (count_zero/2)+1
+	// Case 3: Check for all blocks of 0's after last one
+	// print count_zero
+
+	// members
+	int count_zero;
+	// flag represent cases
+	int FLAG;
+	int answer;
+	int[] arr;
+
+	public void init(int []arr){
+		FLAG = 1;
+		answer = 0;
+		count_zero = 0;
+		
+		//fillOnesInArray(arr);
+	}
+
+	public int fillOnesInArray(int[] arr) {
+		
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] == 0){
+				count_zero++;
+			}
+			else{
+				if(FLAG == 1){
+					setAnswer(answer, count_zero);
+					FLAG = 2;
+					
+				}
+				else if(FLAG == 2){
+					if(count_zero%2 == 1){
+						setAnswer(answer, (count_zero/2) + 1);
+					}
+					else if(count_zero%2 == 0){
+						setAnswer(answer, count_zero/2);
+					}
+				}
+				count_zero = 0;
+			}
+			
+			if(i== arr.length-1){
+				setAnswer(answer, count_zero);
+			}
+			
+		}
+
+		return answer;
+	}
+	public void setAnswer(int ans, int b){
+		if(ans > b){
+			answer = ans;
+		}
+		else 
+			answer = b;
+	}
+
 }
--- src/Solution.java
+++ src/Solution.java
@@ -3,9 +3,12 @@
 public class Solution {
 
 	public static void main(String[] args) {
-		int[] arr = {0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1};
-		
-		
+		int[] arr = {1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1};
+		FillOnesinArray ob = new FillOnesinArray();
+
+		ob.init(arr);
+		int ans = ob.fillOnesInArray(arr);
+		System.out.print(ans);
 		
 		
 	}
