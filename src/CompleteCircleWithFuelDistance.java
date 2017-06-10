56462272d56fd946c65adc9aa06fe361b823dd49
Gaurav Garg <gauravsngarg@gmail.com>
2017-06-10 15:24:41 +0530

    FuelCircleCheckComplete algo commit

---
+++ src/CompleteCircleWithFuelDistance.java
@@ -0,0 +1,46 @@
+
+public class CompleteCircleWithFuelDistance {
+	static class PetrolPump {
+		private int petrol;
+		private int distance;
+
+		public PetrolPump(int a, int b) {
+			this.petrol = a;
+			this.distance = b;
+
+		}
+	}
+
+	public static void main(String[] args) {
+		PetrolPump arr[] = { new PetrolPump(4, 6), new PetrolPump(6, 5), new PetrolPump(7, 3),
+				new PetrolPump(4, 5) };
+
+		System.out.println(FuelCircleDetect(arr));
+	}
+
+	public static int FuelCircleDetect(PetrolPump p[]) {
+
+		int start = 0;
+		int end = p.length - 1;
+		int n = p.length;
+		int cur_pet = 0;
+		int finalStart = start;
+		while (start != end) {
+			cur_pet = cur_pet + (p[start].petrol - p[start].distance);
+			if (cur_pet < 0) {
+				cur_pet = 0;
+				end = start;
+				start = (start + 1) % n;
+				finalStart = start;
+				if (start == 0) {
+					return -1;
+				}
+			} else {
+				start = (start + 1) % n;
+			}
+		}
+
+		return finalStart + 1;
+	}
+
+}
